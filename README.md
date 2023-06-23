4b9a185dbe2291b1f87b86c58cdf8693f23d2752
Jocilé Serra <jocile@gmail.com>
2023-06-23 15:36:54 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Catalogo
+
+Aplicativo de catálogo de imagens para teste de janelas internas com Java Swing
+
+## Referência
+
+[![Java Swing - Criando Menu e janelas internas](http://img.youtube.com/vi/nVtyaCcZLQI/0.jpg)](https://www.youtube.com/watch?v=nVtyaCcZLQI)
---
+++ src/main/java/com/jocile/catalogo/Catalogo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jocile.catalogo;
+
+/**
+ *
+ * @author joc
+ */
+public class Catalogo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
